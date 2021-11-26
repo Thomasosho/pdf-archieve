d9992f40b459ed0bb9e2063b37aef046be5292be
Thomasosho <tomasosho@gmail.com>
2021-11-26 17:19:15 +0100

    l

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('file');
 });
 
-Route::resource('file', FileController::class, 'https');
+Route::resource('file', FileController::class);
 Route::get('type', [FileController::class, 'type']);
 Route::get('date', [FileController::class, 'date']);
 
