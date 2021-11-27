20f24849f586e3cc470e27170674313b625fb51d
Thomasosho <tomasosho@gmail.com>
2021-11-27 03:48:37 +0100

    docx

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -70,6 +70,24 @@ class FileController extends Controller
             $content = 'null';
         }
 
+        if ($request->file('file')->getClientOriginalExtension() == 'DOCX'||'docx') {
+            $zip = zip_open($file);
+
+            if (!$zip || is_numeric($zip)) return false;
+
+            while ($zip_entry = zip_read($zip)) {
+
+                if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+
+                if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+
+                $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+                zip_entry_close($zip_entry);
+            } // end while
+
+            zip_close($zip);
+        }
+
         if($request->hasFile('file')){
             // Get filename with the extension
             $filenameWithExt = $request->file('file')->getClientOriginalName();
