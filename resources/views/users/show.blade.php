8ea2ed688f2ef34dc0017e89ddbf4e29b2816cf2
Thomasosho <tomasosho@gmail.com>
2021-12-12 19:31:16 +0100

    teni

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -32,7 +32,7 @@
             <strong>Roles:</strong>
             @if(!empty($user->getRoleNames()))
                 @foreach($user->getRoleNames() as $v)
-                    <label class="badge badge-success">{{ $v }}</label>
+                    <label class="badge badge-success" style="color:#000;">{{ $v }}</label>
                 @endforeach
             @endif
         </div>
