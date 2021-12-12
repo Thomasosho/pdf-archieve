3da392a970b712476cfe86b8471f73e4be134275
Thomasosho <tomasosho@gmail.com>
2021-12-12 23:44:44 +0100

    bbj

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,8 +27,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background-color:#4b5320 !important;">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('image/army.png') }}">
-                    <img src="{{asset('')}}" alt="">
+                <a class="navbar-brand" href="/">
+                    <img src="{{ asset('image/army.png') }}" alt="">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
