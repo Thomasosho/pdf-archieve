4239777ae0f0542b12689723761f7f5b6c6833e3
Thomasosho <tomasosho@gmail.com>
2021-12-12 23:46:16 +0100

    the

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background-color:#4b5320 !important;">
             <div class="container">
                 <a class="navbar-brand" href="/">
-                    <img src="{{ asset('image/army.png') }}" alt="">
+                    <img src="{{ asset('image/army.png') }}" alt="Nigerian Army" style="width:25%">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
