2b604eb94549df3f1c2958709d8a0b74c4d2a265
Thomasosho <tomasosho@gmail.com>
2021-12-12 19:27:27 +0100

    liverpool

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -37,7 +37,7 @@
     <td>
       @if(!empty($user->getRoleNames()))
         @foreach($user->getRoleNames() as $v)
-           <label class="badge badge-success">{{ $v }}</label>
+           <label class="badge badge-primary">{{ $v }}</label>
         @endforeach
       @endif
     </td>
