5ffc2ea7cdcbf1afe217ef3a66ad735322f7a922
Thomasosho <tomasosho@gmail.com>
2021-12-13 10:18:54 +0100

    ff

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -22,32 +22,36 @@
     </div>
 @endif
 
+<section>
+    <div class="container">
+        <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th width="280px">Action</th>
+        </tr>
+            @foreach ($roles as $key => $role)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $role->name }}</td>
+                <td>
+                    <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
+                    @can('role-edit')
+                        <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+                    @endcan
+                    @can('role-delete')
+                        {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                    @endcan
+                </td>
+            </tr>
+            @endforeach
+        </table>
 
-<table class="table table-bordered">
-  <tr>
-     <th>No</th>
-     <th>Name</th>
-     <th width="280px">Action</th>
-  </tr>
-    @foreach ($roles as $key => $role)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-            @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-            @endcan
-        </td>
-    </tr>
-    @endforeach
-</table>
 
+        {!! $roles->render() !!}
 
-{!! $roles->render() !!}
+    </div>
+</section>
 @endsection
