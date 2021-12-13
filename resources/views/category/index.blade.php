2cd62eb5338e3a73477a43ae1f9f7510de650752
Thomasosho <tomasosho@gmail.com>
2021-12-13 07:09:55 +0100

    fact

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="col-md-7 px-5">
                 <form action="/category/store" enctype="multipart/form-data" method="POST">
-                    {{ csrf_field() }} @csrf
+                    {{ csrf_field() }}
                     <div class="row g-3 py-2">
                         <div class="col-sm">
                             <input type="text" class="form-control" name="name" placeholder="Category Name" aria-label="Category Name">
